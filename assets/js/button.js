58f598131ce17568c99ad04101eab04032e36b27
meglanghoffdesign <meglanghoff@gmail.com>
2025-01-23 22:34:37 -0700

    Added locked functioanlity, add and delete functionality

--- assets/js/button.js
+++ assets/js/button.js
@@ -2,32 +2,5 @@
 const redirectButton = document.getElementById("redirectButton");
 
 redirectButton.addEventListener("click", function () {
-    window.location.href = "index.html"; // Redirects to index.html
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-
-// Event listener to handle hex code input change
-const colorInputs = document.querySelectorAll("input[type='text']");
-colorInputs.forEach(input => {
-     input.addEventListener("input", handleColorInput);
-});
-
-// Event Listener to copy button
-const copyButtons = document.querySelectorAll(".copybtn");
-copyButtons.forEach(button => {
-    button.addEventListener("click", function() {
-        const inputField = this.closest("form").querySelector("input[type='text']");
-        if (inputField) {
-            inputField.select();
-            inputField.setSelectionRange(0, 99999); // For mobile devices
-            navigator.clipboard.writeText(inputField.value).then(() => {
-                alert("Color Copied!");
-            }).catch(err => {
-                console.error('Failed to copy: ', err);
-            });
-        }
-    });
-});
-
+    window.location.href = "index.html"; 
 });
